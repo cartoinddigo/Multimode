fa23d895ae2f903df6df2cf3a88947537bde9014
cartoinddigo <34676078+cartoinddigo@users.noreply.github.com>
2018-02-12 17:24:54 +0100

    Update key.py

--- key.py
+++ key.py
@@ -1,11 +1,8 @@
-"""
-User : carto.inddigo@gmail.com
-pw : groupecarto
-"""
 
-keytpvp = "AIzaSyBBCm4H07TgJsG1QAU-nunQwne6Pm4RA20" 
-keytrafic = "AIzaSyCEbTTbWy42PwyBDOkZ5DvjcisFz-tnNwg"
-keytpvelo = "AIzaSyCyOKSwdUOLW8UT69qS9WsW2Slx5XdSvQs"
-keytpmarche = "AIzaSyC1Zg9I4iUDBkYPHb2HgtACj54UwqHOqog"
-token = "13ae45dd-3938-47a1-a6d5-4b8a2bd58836" #Clé Navitia.io
-keytptransit = "AIzaSyDMr92AYLn37VCX9fXf31sgps9_xgueH20"
+
+keytpvp = "" 
+keytrafic = ""
+keytpvelo = ""
+keytpmarche = ""
+token = "" #Clé Navitia.io
+keytptransit = ""
